cb551d8297a23394713fd5831be1c55a04626815
Dmitrii <kislenko1910@mail.ru>
2019-01-18 10:44:22 +0200

    Made menu

--- first.php
+++ first.php
@@ -1,14 +1,5 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="utf-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Page Title</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
-    <script src="main.js"></script>
-</head>
-<body>
+<?php include "menu.php";?>
+
     <h1>PHP basics</h1>
     <?php
     $myname="Dmitrii";
@@ -48,6 +39,4 @@
     } 
     echo'</ul>';    
      ?>
-     
-</body>
-</html>
+<?php include "footer.php";?>
