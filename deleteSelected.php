b3466a6ae86a1efbbaf2c82e0d7c6a74efb2c8ee
Dmitrii <kislenko1910@mail.ru>
2019-02-06 09:38:12 +0200

    Delete function added

---
+++ deleteSelected.php
@@ -0,0 +1,13 @@
+<?php include "menu.php";?>
+<?php include "connection.php";?>
+<?php
+
+$stmt=$db->prepare("DELETE FROM books WHERE 
+books_id=:idBook");
+		$stmt->bindParam(':idBook', $b_id);
+	    $b_id=$_POST['b_id'];
+
+    
+        $stmt->execute();
+?>
+<?php include "footer.php";?>
